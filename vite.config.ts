f2d83391365fc7fdb08c03a8b16b90a432ca9d18
yung algorithm <113615973+yungalgo@users.noreply.github.com>
2026-02-15 17:19:29 -0500

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": "http://localhost:3000",
+      "/ws": {
+        target: "http://localhost:3000",
+        ws: true,
+      },
     },
   },
 });
